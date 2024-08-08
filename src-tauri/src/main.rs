9935741a68f0fdef5dc38d109020260e94b8bb88
Christian Payne <christianpayne522@gmail.com>
2024-08-07 20:52:58 -0700

    First working test of reading a file from the app data directory

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,12 @@
 
 #[macro_use]
 extern crate dotenv_codegen;
+
+// Tauri APIs
+use tauri::api::file::read_string;
+use std::fs;
+
+
 // Ennesults
 pub mod bot;
 pub mod commands;
@@ -13,30 +19,41 @@ use crate::bot::Bot;
 
 #[tokio::main]
 async fn main() {
-    tauri::Builder::default()
-      .manage(Bot::default())
-      .plugin(tauri_plugin_store::Builder::default().build())
-      .plugin(tauri_plugin_window_state::Builder::default().build())
-      .invoke_handler(tauri::generate_handler![
-        crate::commands::say::say,
-        crate::commands::connect_to_channel::connect_to_channel,
-        crate::commands::print_state::print_state,
-        crate::commands::status::status,
-      ])
-      .setup(|app| {
-        // Create store
-        // let mut store = StoreBuilder::new(app.handle(), "./store.bin".parse()?).build();
-        // let _ = store.insert("a".to_string(), json!("b"));
-        // dbg!(store.is_empty());
-        // let _ = store.save();
+  tauri::Builder::default()
+    .manage(Bot::default())
+    .plugin(tauri_plugin_store::Builder::default().build())
+    .plugin(tauri_plugin_window_state::Builder::default().build())
+    .invoke_handler(tauri::generate_handler![
+      crate::commands::say::say,
+      crate::commands::connect_to_channel::connect_to_channel,
+      crate::commands::print_state::print_state,
+      crate::commands::status::status,
+    ])
+    .setup(|app| {
+      // let resource_path = app.path_resolver().resource_dir();
+      let resource_path = app.path_resolver().app_data_dir().expect("Can't resolve resource dir.");
+      let full_path = format!("{}/test.json", resource_path.to_str().expect("Can't convert to str"));
+    
+      dbg!(&full_path);
+      
+      let message: String = fs::read_to_string(&full_path).unwrap();
+      
+      dbg!(message);
+      
 
-        // Bot::rs2js("test message".to_string(), &app);
 
-        println!("App Started!");
-        Ok(())
-      })
-      .run(tauri::generate_context!())
-      .expect("error while running tauri application");
+
+      // app.handle().path_resolver().app_data_dir()
+
+      // let contents = read_string("$DESKTOP/test/test.json");
+
+      // dbg!(contents);
+
+      println!("App Started!");
+      Ok(())
+    })
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
 
 
